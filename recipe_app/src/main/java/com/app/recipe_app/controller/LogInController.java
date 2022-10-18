c3dc0475d6f2c0a2e9d204dd99b7046ec612cc00
Phoenix Armand Ani <PhoenixArmand30@outlook.com>
2022-10-18 14:37:38 -0400

    Added CSS files Added LogIn Controllers

--- recipe_app/src/main/java/com/app/recipe_app/controller/LogInController.java
+++ recipe_app/src/main/java/com/app/recipe_app/controller/LogInController.java
@@ -4,13 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-@RequestMapping("/home")
-public class HomeController {
-    @GetMapping("/homepage")
+@RequestMapping("home")
+public class LogInController {
+    @GetMapping("/login")
     public String testing(){
-        return "home";
+        return "login";
     }
 }
-
