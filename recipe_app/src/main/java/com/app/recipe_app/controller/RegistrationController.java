2fc7d184a497f1c8287b31525085c42374c5983b
Phoenix Armand Ani <PhoenixArmand30@outlook.com>
2022-10-20 01:05:28 -0400

    Update

--- recipe_app/src/main/java/com/app/recipe_app/controller/RegistrationController.java
+++ recipe_app/src/main/java/com/app/recipe_app/controller/RegistrationController.java
@@ -1,13 +1,13 @@
-package com.app.recipe_app.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/home")
-public class RegistrationController {
-
-    @GetMapping("/registration")
-    public String testing() {return "registration";}
-}
+//package com.app.recipe_app.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/home")
+//public class RegistrationController {
+//
+//    @GetMapping("/registration")
+//    public String testing() {return "registration";}
+//}
